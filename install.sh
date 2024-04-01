7d9cbf09cfba7a5d4de5ad7b8de2ff17169cb962
wajahatarajput <wajahatarajput@gmail.com>
2024-04-01 05:06:16 +0500

    install

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Array containing project directories
+projects=("bnb-admin-dashboard" "bnb-student-registration-portal" "qr-admin-server")
+
+# Function to install dependencies for each project
+install_dependencies() {
+  local project_dir="$1"
+  echo "Installing dependencies for $project_dir..."
+  cd "$project_dir" || return
+  npm install
+  cd ..
+}
+
+# Loop through each project directory and install dependencies
+for project in "${projects[@]}"; do
+  install_dependencies "$project"
+done
