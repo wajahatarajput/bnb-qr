71f8f530faed9e8a10d66dd420f8cbc2ca552503
wajahatarajput <wajahatarajput@gmail.com>
2024-12-16 23:59:12 +0500

    fixed student issue

--- bnb-teachers-portal/src/config/index.js
+++ bnb-teachers-portal/src/config/index.js
@@ -1,2 +1,2 @@
-// export const SERVER_URL = 'http://localhost:5000';
-export const SERVER_URL = 'https://2d7kw05k-5000.inc1.devtunnels.ms';
+export const SERVER_URL = 'http://localhost:5000';
+// export const SERVER_URL = 'https://2d7kw05k-5000.inc1.devtunnels.ms';
